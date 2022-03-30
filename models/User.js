c9d7479d66f83ed03d19e66532232c8b2ed8a0c8
unknown <ericbwebdev86@gmail.com>
2022-03-30 11:03:15 -0500

    updated User with references and virtual, added regex validation for email

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const UserSchema = new Schema(
   {
@@ -11,8 +11,24 @@ const UserSchema = new Schema(
     email: {
       type: String,
       unique: true,
-      required: true
+      required: true,
+      trim: true,
+      match: [/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/, 'Please enter an email address.']
     },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+  },
+  {
     toJSON: {
       virtuals: true,
       getters: true
@@ -23,5 +39,8 @@ const UserSchema = new Schema(
 
 
 const User = model('Pizza', UserSchema);
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
 
 module.exports = User;
